ea461c268ef45ebf1bdb7d753930fcb2f1799625
Matii2233 <141762541+Matii2233@users.noreply.github.com>
2024-04-09 18:39:47 -0300

    Delete Ejercicios_clase4-4/ejercicio2_clase4-4 directory

--- Ejercicios_clase4-4/ejercicio2_clase4-4/src/BaseCharacter.java
+++
@@ -1,5 +0,0 @@
-public abstract class BaseCharacter implements Character{
-    private String name;
-    private int baseHealth;
-    private int baseAttack;
-}
--- Ejercicios_clase4-4/ejercicio2_clase4-4/src/Character.java
+++
@@ -1,4 +0,0 @@
-public interface Character {
-    public void atacar();
-    public void defender();
-}
